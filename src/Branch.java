c904081ddd8d8a6cf4010197c863d2db8ef3b92a
Alex <A.Kouda2@newcastle.ac.uk>
2022-03-02 18:51:31 +0000

    Branch changed so that value can be passed to reporting, All reporting functions fully complete and working

--- src/Branch.java
+++ src/Branch.java
@@ -32,15 +32,18 @@ public class Branch extends Sale{
     }
 
     public static void main(String[] args) {
-        Branch branches = new Branch();
+        //Testing
+       /* Branch branches = new Branch();
         branches.SalesListAdd("South Essex","20","al4 321","3","3",3000);
         branches.SalesListAdd("South Essex","5","al1 4qr","1","5",5000);
         branches.SalesListAdd("South Essex","40","asda","10","2",70000);
         branches.SalesListAdd("South Essex","2","ne2 1dt","1","3",300);
         branches.SalesListAdd("New green","4","se16 5lj","3","3",4000);
         branches.ValueCompare(500,"South Essex");
-        branches.HighestSale("South Essex");
-        branches.AverageSale("South Essex");
+        branches.HighestSale("South Essex",false);
+        branches.AverageSale("South Essex",false);
+
+        */
 
     }
     public void SalesListAdd(String branch_name, String house_number,String postcode, String year,String month, int value){
@@ -49,6 +52,7 @@ public class Branch extends Sale{
         setYear(year);
         setMonth(month);
         setValue(value);
+
         sale_list.add(branch_name);
         sale_list.add(getHouse_number());
         sale_list.add(getPostcose());
@@ -74,56 +78,108 @@ public class Branch extends Sale{
             }
         }
         if (found==false){
-            System.out.println("There are no sales higher than that amount in this branch");
+            System.out.println(branch_name+ "has no sales higher than that amount in this branch");
         }
 
     }
-    public void HighestSale(String branch_name){
+    public int HighestSale(String branch_name,boolean integer){
         int looping_size = sale_list.size();
         int compare_value = 0;
         int new_value = 0;
+        if (integer==false){
+            for (int i = 0; i<looping_size;i++){
+                int value_store = value_list.get(i / 5);
+                String sale_store = sale_list.get(i);
+                if (branch_name.equals(sale_store)){
+                    if (compare_value<value_store){
+                        compare_value =value_store;
+                        new_value = value_store;
 
-        for (int i = 0; i<looping_size;i++){
-            int value_store = value_list.get(i / 5);
-            String sale_store = sale_list.get(i);
-            if (branch_name.equals(sale_store)){
-                if (compare_value<value_store){
-                    compare_value =value_store;
-                    new_value = value_store;
-
+                    }
                 }
             }
-        }
-        if (new_value > 0){
-            System.out.println(branch_name+" highest sale was: "+new_value);
+            if (new_value > 0){
+                System.out.println(branch_name+" highest sale was: "+new_value);
+                return 0;
+            }
+            else {
+                System.out.println("This branch has no sales");
+                return 0;
+            }
         }
         else {
-            System.out.println("This branch has no sales");
+            for (int i = 0; i < looping_size; i++) {
+                int value_store = value_list.get(i / 5);
+                String sale_store = sale_list.get(i);
+                if (branch_name.equals(sale_store)) {
+                    if (compare_value < value_store) {
+                        compare_value = value_store;
+                        new_value = value_store;
+
+                    }
+                }
+            }
+            if (new_value > 0) {
+
+                return new_value;
+            } else {
+
+                return 0;
+            }
         }
 
     }
-    public void AverageSale(String branch_name){
+    public int AverageSale(String branch_name,boolean integer){
         int looping_size = sale_list.size();
         int average_divider = 0;
         int new_value = 0;
         boolean found=false;
-        for (int i = 0; i<looping_size;i++){
-            int value_store = value_list.get(i / 5);
-            String sale_store = sale_list.get(i);
-            if (branch_name.equals(sale_store)){
+        if (integer ==false){
+            for (int i = 0; i<looping_size;i++){
+                int value_store = value_list.get(i / 5);
+                String sale_store = sale_list.get(i);
+                if (branch_name.equals(sale_store)){
                     average_divider++;
                     new_value += value_store;
                     found=true;
+                }
+            }
+            int average = new_value/average_divider;
+
+            if (found==true){
+                System.out.println(branch_name+ "Have an average sale of "+average);
+                return 0;
+            }
+            else{
+                System.out.println("This branch has no sales");
+                return 0;
             }
         }
-        int average = new_value/average_divider;
+        else {
+            for (int i = 0; i<looping_size;i++){
+                int value_store = value_list.get(i / 5);
+                String sale_store = sale_list.get(i);
+                if (branch_name.equals(sale_store)){
+                    average_divider++;
+                    new_value += value_store;
+                    found=true;
+                }
+            }
+            int average = new_value/average_divider;
 
-        if (found==true){
-            System.out.println(branch_name+ "Have an average sale of "+average);
+            if (found==true){
+
+                return average;
+            }
+            else{
+
+                return 0;
+            }
+
         }
-        else{
-            System.out.println("This branch has no sales");
-        }
+
+
+
     }
 
 }
--- src/Reporting.java
+++ src/Reporting.java
@@ -1,2 +1,83 @@
-public class Reporting {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Reporting extends Branch{
+    private List<String> branch_name_list = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Reporting branches = new Reporting();
+        branches.addBranch_name("South Essex");
+        branches.addBranch_name("New green");
+        branches.SalesListAdd("South Essex","20","al4 321","3","3",3000);
+        branches.SalesListAdd("South Essex","5","al1 4qr","1","5",5000);
+        branches.SalesListAdd("South Essex","40","asda","10","2",70000);
+        branches.SalesListAdd("South Essex","2","ne2 1dt","1","3",300);
+        branches.SalesListAdd("New green","4","se16 5lj","3","3",4000);
+        branches.HighestAverageBranch();
+        branches.HighestEverSale();
+        branches.HigherSalesThanValue(500);
+    }
+
+    public void addBranch_name(String branch_name){
+        branch_name_list.add(branch_name);
+
+    }
+
+    public List<String> getBranch_name_list() {
+        return branch_name_list;
+    }
+
+    public void HighestAverageBranch(){
+        int looping_size = branch_name_list.size();
+        int comparable_amount = 0;
+        int new_amount = 0;
+        int new_amount_hold = 0;
+        String branch_name = "";
+        for (int i = 0;i<looping_size;i++){
+            new_amount = AverageSale(branch_name_list.get(i),true);
+            if (comparable_amount < new_amount){
+                new_amount_hold = new_amount;
+                comparable_amount = new_amount;
+                branch_name = branch_name_list.get(i);
+
+            }
+
+        }
+        if (new_amount_hold == 0){
+            System.out.println("There are no branches with an average sale");
+        }
+        else {
+            System.out.println(branch_name + " Has the highest average sales with: " +new_amount_hold);
+        }
+    }
+    public void HighestEverSale(){
+        int looping_size = branch_name_list.size();
+        int comparable_amount = 0;
+        int new_amount = 0;
+        int new_amount_hold = 0;
+        String branch_name = "";
+        for (int i = 0;i<looping_size;i++){
+            new_amount = HighestSale(branch_name_list.get(i),true);
+            if (comparable_amount < new_amount){
+                new_amount_hold = new_amount;
+                comparable_amount = new_amount;
+                branch_name = branch_name_list.get(i);
+
+            }
+
+        }
+        if (new_amount_hold == 0){
+            System.out.println("There are no branches with an average sale");
+        }
+        else {
+            System.out.println(branch_name + " Has the highest sale ever with: " +new_amount_hold);
+        }
+    }
+    public  void HigherSalesThanValue(int value_compare){
+        int looping_size = branch_name_list.size();
+        String branch_name = "";
+        for (int i = 0;i<looping_size;i++){
+            ValueCompare(value_compare,branch_name_list.get(i));
+        }
+    }
 }
